e360591601994a7563db6be87ab9fed4fac2fc16
fgokeke <okekefeargod@gmail.com>
2024-03-12 13:50:57 +0000

    worked on the guest user cart

--- ecom_website/store/views.py
+++ ecom_website/store/views.py
@@ -3,49 +3,32 @@ from django.http import JsonResponse
 import json
 import datetime
 from .models import *
+from .utils import cookieCart, cartData, guestOrder
 
 # Create your views here.
 
 def store(request):
-    
-    if request.user.is_authenticated:
-        customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        items = order.orderitem_set.all()
-        cartItems = order.get_cart_items
-    else:
-        items = []
-        order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
-        cartItems = order['get_cart_items']
-    
+    data = cartData(request)
+    cartItems = data['cartItems']
+ 
     products = Product.objects.all()
     context = {'products': products, 'cartItems': cartItems}
     return render(request, 'store/store.html', context)
 
 def cart(request):
-    if request.user.is_authenticated:
-        customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        items = order.orderitem_set.all()
-        cartItems = order.get_cart_items
-    else:
-        items = []
-        order = {'get_cart_total':0, 'get_cart_items':0, 'shipping': False}
-        cartItems = order['get_cart_items']
+    data = cartData(request)
+    cartItems = data['cartItems']
+    order = data['order']
+    items = data['items']
 
     context = {'items':items, 'order':order,  'cartItems': cartItems}
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
-    if request.user.is_authenticated:
-        customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        items = order.orderitem_set.all()
-        cartItems = order.get_cart_items
-    else:
-        items = []
-        order = {'get_cart_total':0, 'get_cart_items':0, 'shipping': False}
-        cartItems = order['get_cart_items'] 
+    data = cartData(request)
+    cartItems = data['cartItems']
+    order = data['order']
+    items = data['items'] 
 
     context = {'items':items, 'order':order, 'cartItems': cartItems}
     return render(request, 'store/checkout.html', context)
@@ -83,23 +66,30 @@ def updateItem(request):
     except Exception as e:
         return JsonResponse({'error': str(e)}, status=500)
     
-    
+#from django.views.decorators.csrf import csrf_exempt
+
+#@csrf_exempt
 def processOrder(request):
     transaction_id = datetime.datetime.now().timestamp()
     data = json.loads(request.body)
     
     if request.user.is_authenticated:
         customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False)  # Unpack the tuple
-        total = float(data['form']['total'])
-        order.transaction_id = transaction_id
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)  # Unpack the tuple 
         
-        if total == order.get_cart_total:  
-            order.complete = True
-        order.save()
+    
+    else:
+        customer, order = guestOrder(request, data)
+
+    total = float(data['form']['total'])
+    order.transaction_id = transaction_id
         
-        if order.shipping == True:
-            ShippingAddress.objects.create(
+    if total == order.get_cart_total:  
+        order.complete = True
+    order.save()
+
+    if order.shipping == True:
+        ShippingAddress.objects.create(
                 customer=customer,
                 order=order,
                 address=data['shipping']['address'],
@@ -107,8 +97,4 @@ def processOrder(request):
                 state=data['shipping']['state'],
                 zipcode=data['shipping']['zipcode'],
             )
-        
-    
-    else:
-        print('user is not logged in...')
     return JsonResponse('Payment complete!', safe=False)
