8d846dcf9abc4682d09897fce64f6303bc381324
Arinzelight <arinzelight2@gmail.com>
2024-03-14 09:08:19 +0100

    configure CORS settings in  Django application to allow requests from https://django-ecom-website-6.onrender.com

--- ecom_website/ecom_website/settings.py
+++ ecom_website/ecom_website/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'store.apps.StoreConfig',
+     'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -56,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'ecom_website.urls'
@@ -137,5 +139,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "https://django-ecom-website-6.onrender.com",
+    # Add any other allowed origins as needed
+]
+
 
 
