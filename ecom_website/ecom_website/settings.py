c1c76a03521cdcd67e71530b94f2463afea6cf20
Arinzelight <arinzelight2@gmail.com>
2024-03-14 07:23:50 +0100

    add django-ecom-website-5.onrender.com to host name

--- ecom_website/ecom_website/settings.py
+++ ecom_website/ecom_website/settings.py
@@ -32,7 +32,8 @@ SECRET_KEY = 'django-insecure-0ap&5f5m^^f63%d+ap7&-upb+#6uoc!5cr!pxdbbi3^x27%!5_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'django-ecom-website-5.onrender.com']
+
 
 
 # Application definition
